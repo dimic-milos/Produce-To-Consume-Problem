d7962bf669cc6a7e6e5de5a3f8972b327eeb5aa6
Milos Dimic <dimic.milos@icloud.com>
2019-04-28 23:32:34 +0200

    Added task description to AppDelegate;

--- MilosDimicProducerConsumer/AppDelegate.swift
+++ MilosDimicProducerConsumer/AppDelegate.swift
@@ -6,6 +6,18 @@
 //  Copyright © 2019 Dimic Milos. All rights reserved.
 //
 
+/**
+ Create a simple iOS-App which shows a table and two buttons below the table ordered horizontal.
+ Clicking the left button creates a new producer.
+ Clicking the right button creates a new consumer.
+ 
+ A producer adds every 3 seconds a new cell in the table.
+ A consumer removes every 4 seconds a cell from the table.
+ A created producer/consumer will never be removed and will do its job forever.
+ 
+ The user can create producer or consumer how often he wants by clicking the specified buttons.
+ */
+
 import UIKit
 
 @UIApplicationMain
